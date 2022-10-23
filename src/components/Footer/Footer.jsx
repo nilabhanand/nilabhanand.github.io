0a29408f8f36520a0d1ec296b9d49fb1d89e351f
nilabhanand <nilabhanand10@gmail.com>
2022-10-23 18:44:24 -0400

    adding connect btn

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,14 @@ import "../../App.css";
 function Footer() {
   return (
     <div className="footer">
+    
       <div className="footer-content">
+      <button
+            className="connect-btn"
+            onClick={() => window.location = 'mailto:nilabhanand10@gmail.com'}
+          >
+            Let's Connect
+          </button>
         <p>Built with passion by Nilabh Anand.</p>
         <p>All rights reserved. ©</p>
       </div>
